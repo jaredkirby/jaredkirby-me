e29ec263b96aff16ca662bdf15b89e62b50724a8
jaredkirby <jpeterkirby@gmail.com>
2023-09-20 21:02:47 -0700

    ToolList Changes

--- components/HeaderSimple/HeaderSimple.tsx
+++ components/HeaderSimple/HeaderSimple.tsx
@@ -1,48 +1,25 @@
 'use client';
-import { useState } from 'react';
-import { Container, Group, Burger, Text } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+
+import { Container, Group, Text } from '@mantine/core';
 import classes from './HeaderSimple.module.css';
-import { ActionToggle } from '../ActionToggle/ActionToggle';
+import { ActionToggle } from '../ActionToggle/ColorModeToggle';
 import { ActionIconSocials } from '../ActionIconSocial/ActionIconSocial';
 
-const links = [
-  { link: '/about', label: 'Features' },
-  { link: '/pricing', label: 'Pricing' },
-];
+import { IconPhoto, IconPrinter, IconCameraSelfie } from '@tabler/icons-react';
+
 
 export function HeaderSimple() {
-  const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
-
-  const items = links.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      data-active={active === link.link || undefined}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-      }}
-    >
-      {link.label}
-    </a>
-  ));
-
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
         <Text size="xl" fw={700} c="teal">
           Jared Kirby
         </Text>
-        <ActionIconSocials />
-        <Group gap={5} visibleFrom="xs">
-          {items}
-        </Group>
 
-        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
-        <ActionToggle />
+        <Group>
+          <ActionIconSocials />
+          <ActionToggle />
+        </Group>
       </Container>
     </header>
   );
--- components/ToolsList/ToolsList.tsx
+++ components/ToolsList/ToolsList.tsx
@@ -1,45 +1,43 @@
-import { Group, Avatar, Text, Accordion } from '@mantine/core';
-import { IconPhoto, IconPrinter, IconCameraSelfie } from '@tabler/icons-react';
+import { Group, Text, Accordion, rem } from '@mantine/core';
+import { IconBriefcase, IconBrain, IconChartHistogram  } from '@tabler/icons-react';
 
 const charactersList = [
   {
-    id: 'bender',
-    image: 'https://img.icons8.com/clouds/256/000000/futurama-bender.png',
-    label: 'Bender Bending Rodríguez',
-    description: 'Fascinated with cooking, though has no sense of taste',
+    id: 'recruit',
+    image: IconBriefcase,
+    label: 'RecruitPilot',
+    description: 'A set of AI tools to automate resume scoring and generate interview questions.',
     content:
       "Bender Bending Rodríguez, (born September 4, 2996), designated Bending Unit 22, and commonly known as Bender, is a bending unit created by a division of MomCorp in Tijuana, Mexico, and his serial number is 2716057. His mugshot id number is 01473. He is Fry's best friend.",
   },
 
   {
-    id: 'carol',
-    image: 'https://img.icons8.com/clouds/256/000000/futurama-mom.png',
-    label: 'Carol Miller',
-    description: 'One of the richest people on Earth',
+    id: 'smart',
+    image: IconBrain,
+    label: 'SmartPilot',
+    description: "A Python program leveraging OpenAI's language models to generate, analyze, and select the best answer to a given question.",
     content:
-      "Carol Miller (born January 30, 2880), better known as Mom, is the evil chief executive officer and shareholder of 99.7% of Momcorp, one of the largest industrial conglomerates in the universe and the source of most of Earth's robots. She is also one of the main antagonists of the Futurama series.",
+      "SmartPilot is a Python program that generates, analyzes, and selects the best answer to a given question. It leverages the power of OpenAI's language model and a series of prompt-engineered AI models to provide high-quality, reliable, and accurate responses.",
   },
 
   {
-    id: 'homer',
-    image: 'https://img.icons8.com/clouds/256/000000/homer-simpson.png',
-    label: 'Homer Simpson',
-    description: 'Overweight, lazy, and often ignorant',
+    id: 'stats',
+    image: IconChartHistogram,
+    label: 'StatsPilot',
+    description: 'Stats Class Survival Tools',
     content:
-      'Homer Jay Simpson (born May 12) is the main protagonist and one of the five main characters of The Simpsons series(or show). He is the spouse of Marge Simpson and father of Bart, Lisa and Maggie Simpson.',
+      'StatsPilot is a collection of tools designed to assist with statistics class survival. Whether you need help answering an exam question or understanding a statistics concept, StatsPilot has got you covered. This README file provides an overview of the code and its functionalities.',
   },
 ];
 
 interface AccordionLabelProps {
   label: string;
-  image: string;
   description: string;
 }
 
-function AccordionLabel({ label, image, description }: AccordionLabelProps) {
+function AccordionLabel({ label, description }: AccordionLabelProps) {
   return (
     <Group wrap="nowrap">
-      <Avatar src={image} radius="xl" size="lg" />
       <div>
         <Text>{label}</Text>
         <Text size="sm" c="dimmed" fw={400}>
@@ -53,7 +51,7 @@ function AccordionLabel({ label, image, description }: AccordionLabelProps) {
 export function ToolsList() {
   const items = charactersList.map((item) => (
     <Accordion.Item value={item.id} key={item.label}>
-      <Accordion.Control>
+      <Accordion.Control icon={<item.image style={{ color: 'var(--mantine-color-teal-filled', width: rem(50), height: rem(50) }}/>}>
         <AccordionLabel {...item} />
       </Accordion.Control>
       <Accordion.Panel>
@@ -63,7 +61,7 @@ export function ToolsList() {
   ));
 
   return (
-    <Accordion chevronPosition="right" variant="contained">
+    <Accordion chevronPosition="right" variant="filled">
       {items}
     </Accordion>
   );
