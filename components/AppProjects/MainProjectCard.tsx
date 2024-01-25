a1b4f2740b46c59f78af65f8789066b1d45ce223
jaredkirby <jpeterkirby@gmail.com>
2024-01-25 15:20:12 -0800

    Refactor MainProjectCard component

--- components/AppProjects/MainProjectCard.tsx
+++ components/AppProjects/MainProjectCard.tsx
@@ -1,11 +1,9 @@
 'use client';
 import { Card, Tabs, rem, Container } from '@mantine/core';
-import { DocQACard } from './pdfPilot/DocQACard';
-import { DOSToolCard } from './RolePilot/DOSToolCard';
-import { JobToolCard } from './ResumePilot/JobToolCard';
 import { IconBriefcase, IconFileSearch, IconUserCheck, IconAlarmSnooze } from '@tabler/icons-react';
 import classes from './MainProjectCard.module.css';
 import { SleepyPandaApp } from './SleepyPanda/HomeApp/HomeApp';
+import { HomeworkHelperApp } from './HomeworkHelper/HomeworkHelper';
 
 export function MainProjectCard() {
   const iconStyle = { width: rem(15), height: rem(15) };
@@ -13,33 +11,21 @@ export function MainProjectCard() {
   return (
     <Container size="md">
       <Card shadow="md" padding="md" radius="lg" className={classes.card}>
-        <Tabs color="teal" defaultValue="SleepyPanda">
+        <Tabs color="teal" defaultValue="HomeworkHelper">
           <Tabs.List grow justify="center">
-            <Tabs.Tab value="SleepyPanda" leftSection={<IconAlarmSnooze style={iconStyle} />}>
+            <Tabs.Tab value="HomeworkHelper" leftSection={<IconAlarmSnooze style={iconStyle} />}>
+              HomeworkHelper
+            </Tabs.Tab>
+            <Tabs.Tab value="SleepyPanda" leftSection={<IconBriefcase style={iconStyle} />}>
               SleepyPanda
-            </Tabs.Tab>
-            <Tabs.Tab value="JobPilot" leftSection={<IconBriefcase style={iconStyle} />}>
-              JobPilot
-            </Tabs.Tab>
-            <Tabs.Tab value="DocPilot" leftSection={<IconFileSearch style={iconStyle} />}>
-              DocPilot
-            </Tabs.Tab>
-            <Tabs.Tab value="RolePilot" leftSection={<IconUserCheck style={iconStyle} />}>
-              RolePilot
             </Tabs.Tab>
           </Tabs.List>
 
+          <Tabs.Panel value="HomeworkHelper">
+            <HomeworkHelperApp />
+          </Tabs.Panel>
           <Tabs.Panel value="SleepyPanda">
             <SleepyPandaApp />
-          </Tabs.Panel>
-          <Tabs.Panel value="JobPilot">
-            <JobToolCard />
-          </Tabs.Panel>
-          <Tabs.Panel value="DocPilot">
-            <DocQACard />
-          </Tabs.Panel>
-          <Tabs.Panel value="RolePilot">
-            <DOSToolCard />
           </Tabs.Panel>
         </Tabs>
       </Card>
